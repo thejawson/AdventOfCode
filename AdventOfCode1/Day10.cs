ea95c5dabecd723f19cc5d9a3c830c268a99f670
Jawson <john@lawson.pw>
2022-12-10 14:40:50 -0800

    Moved input to seperate class.

--- AdventOfCode1/Day10.cs
+++ AdventOfCode1/Day10.cs
@@ -1,40 +1,22 @@
 ﻿using System.Text;
 
 namespace AdventOfCode;
-class Day10 : IDay
+
+internal class Day10 : IDay
 {
-    private readonly IEnumerable<string[]> Commands = Input.Split("\r\n").Select(x => x.Split(' '));
-    private int Cycles = 0;
-    private readonly Dictionary<int, int> RegistryHistory = new() { { 0, 1 } };
     private const char LiteChar = (char)166;
+    private readonly IEnumerable<string[]> Commands = Input.Day10.Split("\r\n").Select(x => x.Split(' '));
+    private readonly Dictionary<int, int> RegistryHistory = new() { { 0, 1 } };
+    private int Cycles = 0;
 
     public Day10() => Commands.ToList().ForEach(m => AddToHistory(m));
-    
+
     public string Puzzle1() => $"{(RegistryHistory[20] * 20) +
             (RegistryHistory[60] * 60) +
             (RegistryHistory[100] * 100) +
             (RegistryHistory[140] * 140) +
             (RegistryHistory[180] * 180) +
             (RegistryHistory[220] * 220)}";
-
-    private void AddToHistory(string[] command)
-    {
-        switch (command[0])
-        {
-            case "noop":
-                Cycles++;
-                if (!RegistryHistory.ContainsKey(Cycles))
-                    RegistryHistory[Cycles] = RegistryHistory[Cycles - 1];
-                break;
-            case "addx":
-                if (!RegistryHistory.ContainsKey(Cycles + 1))
-                    RegistryHistory[Cycles + 1] = RegistryHistory[Cycles];
-                RegistryHistory[Cycles + 2] = RegistryHistory[Cycles + 1];
-                RegistryHistory[Cycles + 3] = RegistryHistory[Cycles+2] + int.Parse(command[1]);
-                Cycles += 2;
-                break;
-        }
-    }
 
     public string Puzzle2()
     {
@@ -53,144 +35,23 @@ class Day10 : IDay
         return output.ToString();
     }
 
-    private const string Input = @"noop
-noop
-noop
-addx 4
-addx 3
-addx 3
-addx 3
-noop
-addx 2
-addx 1
-addx -7
-addx 10
-addx 1
-addx 5
-addx -3
-addx -7
-addx 13
-addx 5
-addx 2
-addx 1
-addx -30
-addx -8
-noop
-addx 3
-addx 2
-addx 7
-noop
-addx -2
-addx 5
-addx 2
-addx -7
-addx 8
-addx 2
-addx 5
-addx 2
-addx -12
-noop
-addx 17
-addx 3
-addx -2
-addx 2
-noop
-addx 3
-addx -38
-noop
-addx 3
-addx 4
-noop
-addx 5
-noop
-noop
-noop
-addx 1
-addx 2
-addx 5
-addx 2
-addx -3
-addx 4
-addx 2
-noop
-noop
-addx 7
-addx -30
-addx 31
-addx 4
-noop
-addx -24
-addx -12
-addx 1
-addx 5
-addx 5
-noop
-noop
-noop
-addx -12
-addx 13
-addx 4
-noop
-addx 23
-addx -19
-addx 1
-addx 5
-addx 12
-addx -28
-addx 19
-noop
-addx 3
-addx 2
-addx 5
-addx -40
-addx 4
-addx 32
-addx -31
-noop
-addx 13
-addx -8
-addx 5
-addx 2
-addx 5
-noop
-noop
-noop
-addx 2
-addx -7
-addx 8
-addx -7
-addx 14
-addx 3
-addx -2
-addx 2
-addx 5
-addx -40
-noop
-noop
-addx 3
-addx 4
-addx 1
-noop
-addx 2
-addx 5
-addx 2
-addx 21
-noop
-addx -16
-addx 3
-noop
-addx 2
-noop
-addx 1
-noop
-noop
-addx 4
-addx 5
-noop
-noop
-noop
-noop
-noop
-noop
-noop";
+    private void AddToHistory(string[] command)
+    {
+        switch (command[0])
+        {
+            case "noop":
+                Cycles++;
+                if (!RegistryHistory.ContainsKey(Cycles))
+                    RegistryHistory[Cycles] = RegistryHistory[Cycles - 1];
+                break;
+
+            case "addx":
+                if (!RegistryHistory.ContainsKey(Cycles + 1))
+                    RegistryHistory[Cycles + 1] = RegistryHistory[Cycles];
+                RegistryHistory[Cycles + 2] = RegistryHistory[Cycles + 1];
+                RegistryHistory[Cycles + 3] = RegistryHistory[Cycles + 2] + int.Parse(command[1]);
+                Cycles += 2;
+                break;
+        }
+    }
 }
