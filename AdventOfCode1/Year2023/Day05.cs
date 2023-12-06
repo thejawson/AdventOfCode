cf605645c5e07654a5aa8a630439e3972823027c
John Lawson <John_e_lawson@rl.gov>
2023-12-05 17:20:21 -0800

    Started day 5

---
+++ AdventOfCode1/Year2023/Day05.cs
@@ -0,0 +1,45 @@
+﻿namespace AdventOfCode.Year2023
+{
+    internal class Day05 : IDay
+    {
+        private static readonly bool UseTestDate = false;
+        private readonly List<(int[], int[])> cards;
+        public Day05()
+        {
+            var input = UseTestDate
+                ? Input.Day05Test.Split("\n\n").ToList()
+                : Input.Day05.Split("\n\n").ToList();
+        }
+
+        public string Puzzle1()
+        {
+            int sum = 0;
+            foreach (var card in cards)
+                sum += (int)Math.Pow(2, (card.Item1.Count(m => card.Item2.Any(n => m == n)) - 1));
+
+            return sum.ToString();
+        }
+
+        public string Puzzle2()
+        {
+            long sum = 0;
+            Dictionary<int, int> copies = new();
+            for (int i = 0; i < cards.Count; i++)
+                copies[i] = 1;
+            for (int i = 0; i < cards.Count; i++)
+            {
+                int count = cards[i].Item1.Count(m => cards[i].Item2.Any(n => m == n));
+                sum += copies[i];
+                for (int j = count + i; j > i; j--)
+                    copies[j] += copies[i];
+            }
+            return sum.ToString();
+        }
+
+        private static (int[], int[]) ParseCards(string input)
+        {
+            var set = input.Replace("  ", " ").Split(": ")[1].Split(" | ");
+            return (set[0].Split(' ').Select(m => int.Parse(m)).ToArray(), set[1].Split(' ').Select(m => int.Parse(m)).ToArray());
+        }
+    }
+}
--- AdventOfCode1/Year2023/Year.cs
+++ AdventOfCode1/Year2023/Year.cs
@@ -14,7 +14,7 @@ namespace AdventOfCode.Year2023
                 new Day02(),
                 new Day03(),
                 new Day04(),
-                //new Day05(),
+                new Day05(),
                 //new Day06(),
                 //new Day07(),
                 //new Day08(),
